f245d66fb682b12b0fcdfad554403e2aef0c231f
urix.salomon <urix.salomon@gmail.com>
2025-04-16 09:26:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+const apiUrl = 'https://cable1-production.up.railway.app';
 
 app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+
 });
