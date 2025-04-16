9199d7a6eec86d42cf04708c0fe816d8a142c28d
urix.salomon <urix.salomon@gmail.com>
2025-04-16 08:22:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: { rejectUnauthorized: false }
+});
