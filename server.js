c42c0533a4d62daad7014b01fbcfddd63a048618
urix.salomon <urix.salomon@gmail.com>
2025-04-16 08:24:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-const { Pool } = require('pg');
-
 const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
